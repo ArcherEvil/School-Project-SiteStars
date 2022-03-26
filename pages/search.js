cfc3a10ace8489184de48a48a88b0682addf8ea1
Archer <archerevil2005@gmail.com>
2022-03-26 14:05:53 +0000

    Finally

--- pages/search.js
+++ pages/search.js
@@ -7,7 +7,8 @@ import Image from 'next/image'
 import 'aos/dist/aos.css'
 const Search = ({ data }) => {
     const router = useRouter();
-    const fruit = router.query.name.toLowerCase()
+    let fruit = router.query.name
+    fruit = fruit?.toLowerCase()
     let results = []
     for (var i = 0; i < data.length; i++) {
         if (unidecode(data[i].Name.toLowerCase()).includes(fruit) && !results.includes(data[i])) {
